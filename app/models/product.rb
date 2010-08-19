13251943105f23ec09d18ff435ea7f72bd910b1c
shiba_yu36 <shiba1029196473@gmail.com>
2010-08-19 23:09:30 +0900

    productページ

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,23 @@
 class Product < ActiveRecord::Base
-    validates_presence_of :title, :description, :image_url
-    validates_numericality_of :price
-    validate :price_must_be_at_least_a_cent
-    validates_uniqueness_of :title
-    validates_format_of(
-        :image_url,
-        :with    => %r{\.(gif|jpg|png)$}i,
-        :message => 'はGIF、JPG、PNG画像のURLでなければなりません'
-    )
+
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
+
+  validates_presence_of :title, :description, :image_url
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
+  validates_uniqueness_of :title
+  validates_format_of(
+    :image_url,
+    :with    => %r{\.(gif|jpg|png)$}i,
+    :message => 'はGIF、JPG、PNG画像のURLでなければなりません'
+  )
 
 
-
-
-    protected
-    def price_must_be_at_least_a_cent
-        errors.add(:price, 'は最小でも0.01以上でなければなりません') if price.nil? ||
-            price < 0.01
-    end
+  protected
+  def price_must_be_at_least_a_cent
+    errors.add(:price, 'は最小でも0.01以上でなければなりません') if price.nil? ||
+      price < 0.01
+  end
 end
