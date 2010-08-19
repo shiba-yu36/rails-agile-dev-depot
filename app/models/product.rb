33464f8d7dada15f8f8ba2b4dad99179e2300fa4
shiba_yu36 <shiba1029196473@gmail.com>
2010-08-19 10:58:07 +0900

    productsビューを作成

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ActiveRecord::Base
+    validates_presence_of :title, :description, :image_url
+    validates_numericality_of :price
+    validate :price_must_be_at_least_a_cent
+    validates_uniqueness_of :title
+    validates_format_of :image_url,
+                        :with    => %r{ \.(gif|jpg|png)$}i,
+                        :message => 'はGIF、JPG、PNG画像のURLでなければなりません'
+
+
+
+
+    protected
+    def price_must_be_at_least_a_cent
+        errors.add(:price, 'は最小でも0.01以上でなければなりません') if price.nil? ||
+            price < 0.01
+    end
 end
