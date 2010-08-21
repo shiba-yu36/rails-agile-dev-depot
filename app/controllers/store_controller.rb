5610c19ba67901eaa233d301366b9a43a248e690
shiba_yu36 <shiba1029196473@gmail.com>
2010-08-21 20:53:59 +0900

    Ajaxの追加まで

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,35 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
   end
+
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @current_item = @cart.add_product(product)
+    respond_to do |format|
+      format.js
+    end
+  rescue ActiveRecord::RecordNotFound
+    logger.error("無効な商品#{params[:id]}にアクセスしようとしました")
+    redirect_to_index("無効な商品です")
+  end
+
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index
+  end
+
+private
+
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
+    redirect_to :action => 'index'
+  end
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
+
 end
